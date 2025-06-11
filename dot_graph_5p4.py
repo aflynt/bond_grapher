d69f1d78e6783a7b4a3f236389c452dbaaffff03
Austin Flynt <aflynt1@gmail.com>
2025-06-11 15:44:35 -0500

    Refactor node definitions and edge creation for clarity and consistency

--- dot_graph_5p4.py
+++ dot_graph_5p4.py
@@ -1,37 +1,39 @@
 from lib_bonds import *
 graph = pydot.Dot("my_graph", graph_type="digraph", bgcolor="white")
 
-node_SF = FlyNodeSF("SF")
-node_C  = FlyNodeC("C")
-node_R = FlyNodeR("R")
-node_I = FlyNodeI("I")
-node_0 = FlyNodeZERO("0")
-node_1 = FlyNodeONE("1")
+n_SF = "SF"
+n_C  = "C"
+n_R  = "R"
+n_I  = "I"
+n_0  = "0"
+n_1  = "1"
 
 
 # make list of nodes
-ns = [ node_SF, node_C, node_R, node_I, node_0, node_1, ]
+ns = [ n_SF, n_C, n_R, n_I, n_0, n_1, ]
 
 # make list of edges
 es = [
-    FlyEdge(1, node_SF.get_name(), node_0.get_name() , pwr_to_dest=1),
-    FlyEdge(2, node_C.get_name(), node_0.get_name() , pwr_to_dest=0),
-    FlyEdge(3, node_0.get_name(), node_1.get_name() , pwr_to_dest=1),
-    FlyEdge(4, node_1.get_name(), node_R.get_name() , pwr_to_dest=1),
-    FlyEdge(5, node_1.get_name(), node_I.get_name() , pwr_to_dest=1),
+    FlyEdge(1, n_SF, n_0, pwr_to_dest=1),
+    FlyEdge(2, n_C, n_0, pwr_to_dest=0),
+    FlyEdge(3, n_0, n_1, pwr_to_dest=1),
+    FlyEdge(4, n_1, n_R, pwr_to_dest=1),
+    FlyEdge(5, n_1, n_I, pwr_to_dest=1),
 ]
 
 assign_causality_to_all_nodes(es)
 
-# report edge flow_side
-for e in es:
-    print(f"Edge {e.num} from {e.src} to {e.dest} has flow side: {e.flow_side}")
+generate_symbols(es)
 
+
+'''
 for n in ns:
-    graph.add_node(n.node)
+    graph.add_node(pydot.Node(n, shape="none", label=n))
 
 for e in es:
     graph.add_edge(e.mk_edge())
 
-# graph.write_png("graph_5p4.png") # type: ignore
-graph.write_pdf("o.pdf") # type: ignore
+graph.write_png("graph_5p4.png") # type: ignore
+# graph.write_pdf("o.pdf") # type: ignore
+
+'''
